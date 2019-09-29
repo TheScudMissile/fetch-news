edd3f3bdef0b3de2515570278e76fa8b12e3af30
TheScudMissile <SSscuds37@gmail.com>
2019-09-28 20:19:23 -0500

    Added header component

--- App.tsx
+++ App.tsx
@@ -1,19 +1,11 @@
 import * as React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
+import Header from './src/components/Header';
 
 export default () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View>
+      <Header />
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    alignItems: 'center',
-    backgroundColor: '#fff',
-    flex: 1,
-    justifyContent: 'center',
-  },
-});
--- src/components/Header.test.tsx
+++ src/components/Header.test.tsx
@@ -1,10 +1,10 @@
 import * as React from 'react';
 import { create } from 'react-test-renderer';
-import App from './App';
+import Header from './Header';
 
-describe('App', () => {
+describe('Header', () => {
   test('should render correctly', () => {
-    const tree = create(<App />).toJSON();
+    const tree = create(<Header />).toJSON();
     expect(tree).toMatchSnapshot();
   });
 });
