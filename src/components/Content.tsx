e1c1632250637d9a7cab69605d18b59e1d67da2b
TheScudMissile <SSscuds37@gmail.com>
2019-10-01 21:05:39 -0500

    Fetch news data and place in card list

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,25 +1,50 @@
-import * as React from 'react';
+import React, { useEffect, useState } from 'React';
 import { Body, Card, CardItem, Content, Left, Text } from 'native-base';
+import { NEWS_API_KEY } from '../constants';
+import { Article } from '../types';
 
-export default () => (
-  <Content padder>
-    {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11].map((num: number) => (
-      <Card
-        key={num}
-        style={{ marginBottom: 15, marginLeft: 15, marginRight: 15 }}
-      >
-        <CardItem>
-          <Left>
-            <Body>
-              <Text>
-                This is some dummy content going on my card. This is some dummy
-                content going on my card. This is some dummy content going on my
-                card. This is some dummy content going on my card.
-              </Text>
-            </Body>
-          </Left>
-        </CardItem>
-      </Card>
-    ))}
-  </Content>
-);
+export default () => {
+  const [newsArticles, setNewsArticles] = useState<Article[]>([]);
+
+  useEffect(() => {
+    const fetchNews = async () => {
+      const headlinesUrl = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${NEWS_API_KEY}`;
+
+      try {
+        const response = await fetch(headlinesUrl);
+        const data = await response.json();
+        setNewsArticles(data.articles);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchNews();
+  }, []);
+
+  return (
+    <Content padder>
+      {newsArticles.map(article => (
+        <Card
+          key={article.url}
+          style={{ marginBottom: 15, marginLeft: 15, marginRight: 15 }}
+        >
+          <CardItem>
+            <Left>
+              <Body>
+                <Text style={{ marginBottom: 15 }}>Title: {article.title}</Text>
+                <Text style={{ marginBottom: 15 }}>
+                  Description: {article.description}
+                </Text>
+                <Text style={{ marginBottom: 15 }}>
+                  Published: {article.publishedAt}
+                </Text>
+                <Text style={{ marginBottom: 15 }}>Article: {article.url}</Text>
+              </Body>
+            </Left>
+          </CardItem>
+        </Card>
+      ))}
+    </Content>
+  );
+};
---
+++ src/constants.ts
@@ -0,0 +1,1 @@
+export const NEWS_API_KEY = 'bda3d002b7f1420fa6dbb89f8d9541c3';
