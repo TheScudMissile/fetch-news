20852346a624f164c344e7a22c46d94ff573f8c3
TheScudMissile <SSscuds37@gmail.com>
2019-10-02 21:07:31 -0500

    Added full article linking, better error handling

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,25 +1,55 @@
 import React, { useEffect, useState } from 'React';
-import { Body, Card, CardItem, Content, Left, Text } from 'native-base';
+import {
+  Body,
+  Button,
+  Card,
+  CardItem,
+  Content,
+  Left,
+  Text,
+  Toast,
+} from 'native-base';
 import { NEWS_API_KEY } from '../constants';
 import { Article } from '../types';
+import axios from 'axios';
+import { Linking } from 'react-native';
+
+const fetchTopHeadlines = async (
+  setNewsArticles: (articles: Article[]) => void
+) => {
+  try {
+    const response = await axios.get(
+      `https://newsapi.org/v2/top-headlines?country=us&apiKey=${NEWS_API_KEY}`
+    );
+    setNewsArticles(response.data.articles);
+  } catch (error) {
+    console.error(error);
+    Toast.show({
+      buttonText: 'OK',
+      duration: 3000,
+      text: 'Failed to load articles.',
+    });
+  }
+};
+
+const navigateToArticle = (url: string) => {
+  try {
+    Linking.openURL(url);
+  } catch (error) {
+    console.error(error);
+    Toast.show({
+      buttonText: 'OK',
+      duration: 3000,
+      text: 'Unable to view article.',
+    });
+  }
+};
 
 export default () => {
   const [newsArticles, setNewsArticles] = useState<Article[]>([]);
 
   useEffect(() => {
-    const fetchNews = async () => {
-      const headlinesUrl = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${NEWS_API_KEY}`;
-
-      try {
-        const response = await fetch(headlinesUrl);
-        const data = await response.json();
-        setNewsArticles(data.articles);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    fetchNews();
+    fetchTopHeadlines(setNewsArticles);
   }, []);
 
   return (
@@ -39,7 +69,14 @@ export default () => {
                 <Text style={{ marginBottom: 15 }}>
                   Published: {article.publishedAt}
                 </Text>
-                <Text style={{ marginBottom: 15 }}>Article: {article.url}</Text>
+                <Button
+                  color="#34baeb"
+                  transparent
+                  style={{ marginLeft: 0 }}
+                  onPress={() => navigateToArticle(article.url)}
+                >
+                  <Text style={{ paddingLeft: 0 }}>Full Article</Text>
+                </Button>
               </Body>
             </Left>
           </CardItem>
